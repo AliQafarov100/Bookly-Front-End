2bbb72c5cf05711c59685a2b88e5b0580005ed42
Ali <tu8cvbxnx@code.edu.az>
2022-07-12 01:19:09 +0400

    Initial commit

---
+++ assets/Js/script.js
@@ -0,0 +1,75 @@
+let category = document.querySelector(".category");
+let books = document.querySelector(".fluid");
+
+$(document).ready(function(){
+    $(".all").show();
+    $(".cook").hide();
+    $(".history").hide();
+    $(".fantacy").hide(); 
+    $(".romance").hide();   
+})
+
+$(document).ready(function(){
+    $(".all-category").click(function(){
+        $(".all").show();
+        $(".cook").hide();
+        $(".history").hide();
+        $(".fantacy").hide();
+        $(".romance").hide();
+    })
+})
+
+$(document).ready(function(){
+    $(".cook-category").click(function(){
+        $(".cook").show();
+        $(".all").hide();
+        $(".history").hide();
+        $(".fantacy").hide();
+        $(".romance").hide();
+    })
+})
+$(document).ready(function(){
+    $(".history-category").click(function(){
+        $(".history").show();
+        $(".cook").hide();
+        $(".all").hide();
+        $(".fantacy").hide();
+        $(".romance").hide();
+    })
+})
+$(document).ready(function(){
+    $(".fantacy-category").click(function(){
+        $(".fantacy").show();
+        $(".cook").hide();
+        $(".all").hide();
+        $(".history").hide();
+        $(".romance").hide();
+    })
+})
+
+$(document).ready(function(){
+    $(".romance-category").click(function(){
+        $(".romance").show();
+        $(".cook").hide();
+        $(".all").hide();
+        $(".history").hide();
+        $(".fantacy").hide();
+    })
+})
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
