36d7a487c00623d34b4f35cdeb27d455d998bea3
Ali <tu8cvbxnx@code.edu.az>
2022-07-15 23:45:52 +0400

    Update commit

--- assets/Js/product.js
+++ assets/Js/product.js
@@ -2,6 +2,56 @@ let bookItem = document.querySelectorAll(".slider");
 let sliders = document.querySelector(".sliders");
 let count = 0;
 let width;
+let plus = document.querySelector(".plus");
+let minus = document.querySelector(".minus");
+let quantity = document.querySelector(".quantity");
+let counter = 0;
+let arrow = document.querySelector(".circle");
+let image = document.querySelectorAll(".another");
+ 
+
+image.forEach(images => {
+    images.addEventListener("click",function(){
+        let id = images.getAttribute("data-id");
+        let mainImage = document.querySelectorAll(".img-zoom");
+
+        mainImage.forEach(mains => {
+            let mainId = mains.getAttribute("data-id");
+    
+            if(mainId == id){
+                mains.classList.add("active")
+            }
+            else{
+                mains.classList.remove("active");
+            }
+        })
+       
+    })
+})
+window.addEventListener("scroll",function(){
+    if(window.scrollY == 0){
+        arrow.style.opacity = "0";
+    }
+    else{
+        arrow.style.opacity = "1";
+    }
+})
+
+plus.addEventListener("click",function(){
+     counter++;
+     quantity.innerHTML = counter;
+     console.log(quantity.innerHTML)
+});
+
+minus.addEventListener("click",function(){
+    counter--;
+
+    if(counter <= 0){
+        counter = 0;
+    }
+
+    quantity.innerHTML = counter;
+})
 
 function init(){
     width = document.querySelector(".sliderItem").offsetWidth;
