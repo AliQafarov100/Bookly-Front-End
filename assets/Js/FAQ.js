b2d04a57a220cca82f32d5dabad6927666571df6
Ali <tu8cvbxnx@code.edu.az>
2022-07-16 22:04:38 +0400

    Update commit

---
+++ assets/Js/FAQ.js
@@ -0,0 +1,222 @@
+
+$(".text-one").hide();
+$(".hist-minus-one").click(function(){
+    $(".text-one").slideUp();
+    $(".hist-minus-one").css("display","none");
+    $(".hist-plus-one").css("display","block");
+   
+})
+$(".hist-plus-one").click(function(){
+    $(".text-one").slideDown();
+    $(".text-second").slideUp();
+    $(".hist-minus-second").css("display","none");
+    $(".hist-plus-second").css("display","block");
+    $(".text-three").slideUp();
+    $(".hist-minus-three").css("display","none");
+    $(".hist-plus-three").css("display","block");
+    $(".text-four").slideUp();
+    $(".hist-minus-four").css("display","none");
+    $(".hist-plus-four").css("display","block");
+    $(".text-five").slideUp();
+    $(".hist-minus-five").css("display","none");
+    $(".hist-plus-five").css("display","block");
+    $(".text-six").slideUp();
+    $(".hist-minus-six").css("display","none");
+    $(".hist-plus-six").css("display","block");
+    $(".text-seven").slideUp();
+    $(".hist-minus-seven").css("display","none");
+    $(".hist-plus-seven").css("display","block");
+
+    $(".hist-plus-one").css("display","none");
+    $(".hist-minus-one").css("display","block");
+});
+
+$(".text-second").hide();
+$(".hist-minus-second").click(function(){
+    $(".text-second").slideUp();
+    $(".hist-minus-second").css("display","none");
+    $(".hist-plus-second").css("display","block");
+   
+})
+$(".hist-plus-second").click(function(){
+    $(".text-second").slideDown();
+    $(".text-one").slideUp();
+    $(".hist-minus-one").css("display","none");
+    $(".hist-plus-one").css("display","block");
+    $(".text-three").slideUp();
+    $(".hist-minus-three").css("display","none");
+    $(".hist-plus-three").css("display","block");
+    $(".text-four").slideUp();
+    $(".hist-minus-four").css("display","none");
+    $(".hist-plus-four").css("display","block");
+    $(".text-five").slideUp();
+    $(".hist-minus-five").css("display","none");
+    $(".hist-plus-five").css("display","block");
+    $(".text-six").slideUp();
+    $(".hist-minus-six").css("display","none");
+    $(".hist-plus-six").css("display","block");
+    $(".text-seven").slideUp();
+    $(".hist-minus-seven").css("display","none");
+    $(".hist-plus-seven").css("display","block");
+    $(".hist-plus-second").css("display","none");
+    $(".hist-minus-second").css("display","block");
+});
+$(".text-three").hide();
+$(".hist-minus-three").click(function(){
+    $(".text-three").slideUp();
+    $(".hist-minus-three").css("display","none");
+    $(".hist-plus-three").css("display","block");
+   
+})
+$(".hist-plus-three").click(function(){
+    $(".text-three").slideDown();
+    $(".text-second").slideUp();
+    $(".hist-minus-second").css("display","none");
+    $(".hist-plus-second").css("display","block");
+    $(".text-one").slideUp();
+    $(".hist-minus-one").css("display","none");
+    $(".hist-plus-one").css("display","block");
+    $(".text-four").slideUp();
+    $(".hist-minus-four").css("display","none");
+    $(".hist-plus-four").css("display","block");
+    $(".text-five").slideUp();
+    $(".hist-minus-five").css("display","none");
+    $(".hist-plus-five").css("display","block");
+    $(".text-six").slideUp();
+    $(".hist-minus-six").css("display","none");
+    $(".hist-plus-six").css("display","block");
+    $(".text-seven").slideUp();
+    $(".hist-minus-seven").css("display","none");
+    $(".hist-plus-seven").css("display","block");
+
+    $(".hist-plus-three").css("display","none");
+    $(".hist-minus-three").css("display","block");
+});
+
+$(".text-four").hide();
+$(".hist-minus-four").click(function(){
+    $(".text-four").slideUp();
+    $(".hist-minus-four").css("display","none");
+    $(".hist-plus-four").css("display","block");
+   
+})
+$(".hist-plus-four").click(function(){
+    $(".text-four").slideDown();
+    $(".text-second").slideUp();
+    $(".hist-minus-second").css("display","none");
+    $(".hist-plus-second").css("display","block");
+    $(".text-three").slideUp();
+    $(".hist-minus-three").css("display","none");
+    $(".hist-plus-three").css("display","block");
+    $(".text-one").slideUp();
+    $(".hist-minus-one").css("display","none");
+    $(".hist-plus-one").css("display","block");
+    $(".text-five").slideUp();
+    $(".hist-minus-five").css("display","none");
+    $(".hist-plus-five").css("display","block");
+    $(".text-six").slideUp();
+    $(".hist-minus-six").css("display","none");
+    $(".hist-plus-six").css("display","block");
+    $(".text-seven").slideUp();
+    $(".hist-minus-seven").css("display","none");
+    $(".hist-plus-seven").css("display","block");
+
+    $(".hist-plus-four").css("display","none");
+    $(".hist-minus-four").css("display","block");
+});
+
+$(".text-five").hide();
+$(".hist-minus-five").click(function(){
+    $(".text-five").slideUp();
+    $(".hist-minus-five").css("display","none");
+    $(".hist-plus-five").css("display","block");
+   
+})
+$(".hist-plus-five").click(function(){
+    $(".text-five").slideDown();
+    $(".text-second").slideUp();
+    $(".hist-minus-second").css("display","none");
+    $(".hist-plus-second").css("display","block");
+    $(".text-three").slideUp();
+    $(".hist-minus-three").css("display","none");
+    $(".hist-plus-three").css("display","block");
+    $(".text-four").slideUp();
+    $(".hist-minus-four").css("display","none");
+    $(".hist-plus-four").css("display","block");
+    $(".text-one").slideUp();
+    $(".hist-minus-one").css("display","none");
+    $(".hist-plus-one").css("display","block");
+    $(".text-six").slideUp();
+    $(".hist-minus-six").css("display","none");
+    $(".hist-plus-six").css("display","block");
+    $(".text-seven").slideUp();
+    $(".hist-minus-seven").css("display","none");
+    $(".hist-plus-seven").css("display","block");
+
+    $(".hist-plus-five").css("display","none");
+    $(".hist-minus-five").css("display","block");
+});
+
+$(".text-six").hide();
+$(".hist-minus-six").click(function(){
+    $(".text-six").slideUp();
+    $(".hist-minus-six").css("display","none");
+    $(".hist-plus-six").css("display","block");
+   
+})
+$(".hist-plus-six").click(function(){
+    $(".text-six").slideDown();
+    $(".text-second").slideUp();
+    $(".hist-minus-second").css("display","none");
+    $(".hist-plus-second").css("display","block");
+    $(".text-three").slideUp();
+    $(".hist-minus-three").css("display","none");
+    $(".hist-plus-three").css("display","block");
+    $(".text-four").slideUp();
+    $(".hist-minus-four").css("display","none");
+    $(".hist-plus-four").css("display","block");
+    $(".text-five").slideUp();
+    $(".hist-minus-five").css("display","none");
+    $(".hist-plus-five").css("display","block");
+    $(".text-one").slideUp();
+    $(".hist-minus-one").css("display","none");
+    $(".hist-plus-one").css("display","block");
+    $(".text-seven").slideUp();
+    $(".hist-minus-seven").css("display","none");
+    $(".hist-plus-seven").css("display","block");
+
+    $(".hist-plus-six").css("display","none");
+    $(".hist-minus-six").css("display","block");
+});
+
+$(".text-seven").hide();
+$(".hist-minus-seven").click(function(){
+    $(".text-seven").slideUp();
+    $(".hist-minus-seven").css("display","none");
+    $(".hist-plus-seven").css("display","block");
+   
+})
+$(".hist-plus-seven").click(function(){
+    $(".text-seven").slideDown();
+    $(".text-second").slideUp();
+    $(".hist-minus-second").css("display","none");
+    $(".hist-plus-second").css("display","block");
+    $(".text-three").slideUp();
+    $(".hist-minus-three").css("display","none");
+    $(".hist-plus-three").css("display","block");
+    $(".text-four").slideUp();
+    $(".hist-minus-four").css("display","none");
+    $(".hist-plus-four").css("display","block");
+    $(".text-five").slideUp();
+    $(".hist-minus-five").css("display","none");
+    $(".hist-plus-five").css("display","block");
+    $(".text-six").slideUp();
+    $(".hist-minus-six").css("display","none");
+    $(".hist-plus-six").css("display","block");
+    $(".text-one").slideUp();
+    $(".hist-minus-one").css("display","none");
+    $(".hist-plus-one").css("display","block");
+
+    $(".hist-plus-seven").css("display","none");
+    $(".hist-minus-seven").css("display","block");
+});
